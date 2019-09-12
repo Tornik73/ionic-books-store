7e61e4f0542917c279b96a28545a92fe124502d2
developer <egor.kotelevets@gmail.com>
2019-09-12 10:18:51 +0300

    fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,17 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./modules/tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  { path: 'profile', loadChildren: 'modules/profile/profile/profile.module#ProfilePageModule' },
-  { path: 'register', loadChildren: 'modules/auth/register/register.module#RegisterPageModule'},
-  { path: 'login', loadChildren: 'modules/auth/login/login.module#LoginPageModule'},
-  { path: 'confirm', loadChildren: 'modules/confirm-email/confirm-email.module#ConfirmEmailPageModule'},
-  { path: 'admin-panel', loadChildren: 'modules/profile/admin-panel/admin-panel.module#AdminPanelPageModule', canActivate: [NotAuthGuard] },
-  { path: 'chat', loadChildren: './chat/chat.module#ChatPageModule', canActivate: [NotAuthGuard] },
-  { path: 'cart', loadChildren: './cart/cart.module#CartPageModule',  canActivate: [NotAuthGuard]  }
+  { path: 'profile', loadChildren: './modules/tabs/profile/profile/profile.module#ProfilePageModule' },
+  { path: 'register', loadChildren: './modules/tabs/auth/register/register.module#RegisterPageModule'},
+  { path: 'login', loadChildren: './modules/tabs/auth/login/login.module#LoginPageModule'},
+  { path: 'confirm', loadChildren: './modules/tabs/confirm-email/confirm-email.module#ConfirmEmailPageModule'},
+  {
+    path: 'admin-panel',
+    loadChildren: './modules/tabs/profile/admin-panel/admin-panel.module#AdminPanelPageModule',
+    canActivate: [NotAuthGuard]
+  },
+  { path: 'chat', loadChildren: './modules/tabs/chat/chat.module#ChatPageModule', canActivate: [NotAuthGuard] },
+  { path: 'cart', loadChildren: './modules/tabs/cart/cart.module#CartPageModule',  canActivate: [NotAuthGuard]  }
 ];
 @NgModule({
   imports: [
